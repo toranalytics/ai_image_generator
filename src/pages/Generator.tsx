62d3b9b2dd4207847b0518aa03b3b943ed0f8985
toranalytics <tjwnsqja0704@gmail.com>
2025-11-12 14:07:57 +0900

    dd

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -363,7 +363,7 @@ ${personRule}
                     
                     {/* [수정 2] <div>를 <label>로 변경하여 스위치 영역을 클릭 가능하게 만듭니다. */}
                     <label className="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" id="include-person" className="sr-only peer" checked={includePerson} onChange={() => setIncludePerson(!includeSerson)} />
+                        <input type="checkbox" id="include-person" className="sr-only peer" checked={includePerson} onChange={() => setIncludePerson(!includePerson)} />
                         <div className="w-11 h-6 bg-slate-600 rounded-full peer peer-focus:ring-4 peer-focus:ring-indigo-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-indigo-600"></div>
                     </label>
                   </div>
